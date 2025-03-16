29060702d5a5edf005e12785b5885b6333565803
Ricardo Monterrsoa <48804047+ricky-antonio@users.noreply.github.com>
2025-03-16 13:39:20 -0700

    Add Notification component

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -6,6 +6,7 @@ import { heroIcons } from "../constants";
 import { ScrollParallax } from "react-just-parallax";
 import { useRef } from "react";
 import Generating from "./Generating";
+import Notification from "./Notification";
 
 const Hero = () => {
     const parallaxRef = useRef(null);
@@ -21,7 +22,7 @@ const Hero = () => {
             <div className="container relative" ref={parallaxRef}>
                 <div className="relative z-1 max-w-[62rem] mx-auto text-center mb-[4rem] md:mb-20 lg:mb-[6rem]">
                     <h1 className="h1 mb-6">
-                        Explore the Possibilities  of&nbsp;AI&nbsp;Chatting with{" "}
+                        Explore the Possibilities of&nbsp;AI&nbsp;Chatting with{" "}
                         <span className="inline-block relative">
                             Brainwave{" "}
                             <img
@@ -60,10 +61,22 @@ const Hero = () => {
                                     <ul className="hidden absolute -left-[5.5rem] bottom-[7.5rem] px-1 py-1 bg-n-9/40 backdrop-blur border border-n-1/10 rounded-2xl xl:flex">
                                         {heroIcons.map((icon, index) => (
                                             <li className="p-5" key={index}>
-                                                <img src={icon} width={24} height={25} alt={icon} />
+                                                <img
+                                                    src={icon}
+                                                    width={24}
+                                                    height={25}
+                                                    alt={icon}
+                                                />
                                             </li>
                                         ))}
                                     </ul>
+                                </ScrollParallax>
+
+                                <ScrollParallax isAbsolutelyPositioned>
+                                    <Notification
+                                        className="hidden absolute -right-[5.5rem] bottom-[11rem] w-[18rem] xl:flex"
+                                        title="code generation"
+                                    />
                                 </ScrollParallax>
                             </div>
                         </div>
